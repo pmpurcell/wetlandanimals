b022590a1bd99fa09b54e0fe2ef476d57b3402f9
pmpurcell93 <maddenpurcell@gmail.com>
2021-07-27 17:55:01 -0500

    Created objects for animal cards

---
+++ main.js
@@ -0,0 +1,44 @@
+const animals = [
+    {
+        name: "Otter",
+        imageUrl: "https://www.theanimalspot.com/wp-content/uploads/2019/01/northamericanriverottersmall.jpg"
+        diet: "Carnivore",
+        class: "mammal",
+    },
+    {
+        name: "Red-Eared Slider",
+        imageUrl: "https://i2.wp.com/envirobites.org/wp-content/uploads/2020/04/red-eared-slider.jpg?resize=640%2C445&ssl=1"
+        diet: "Omnivore",
+        class: "reptile",
+
+    },
+    {
+        name: "Yellow Crowned Night Heron",
+        imageUrl: "https://www.allaboutbirds.org/guide/assets/photo/70693871-480px.jpg"
+        diet: "Omnivore",
+        class: "bird",
+
+    },
+    {
+        name: "Beaver",
+        imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/6b/American_Beaver.jpg"
+        diet: "Omnivore",
+        class: "mammal",
+
+    },
+    {
+        name: "Northern Water Snake",
+        imageUrl: "https://www.raritanheadwaters.org/wp-content/uploads/2020/06/northern-water-snake-1.jpg"
+        diet: "Omnivore",
+        class: "reptile",
+
+    },
+    {
+        name: "Northern Water Snake",
+        imageUrl: "https://cdn11.bigcommerce.com/s-xj69ljw63/images/stencil/935x701/uploaded_images/blog_images/different-forms-of-goose-control.jpg",
+        diet: "Herbivore",
+        class: "bird",
+
+    },
+    
+]
